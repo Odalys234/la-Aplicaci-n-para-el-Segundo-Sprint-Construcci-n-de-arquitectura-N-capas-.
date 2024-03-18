632def9e8299ddc19f6a4c7c4477661418deefcd
katherineodalys24 <katherineodalys24@outlook.es>
2024-03-18 16:53:32 -0600

    comit

---
+++ TOURTRADE.SysLoginTT/TOURTRADE.SysLoginTT.EN/Reservas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOURTRADE.SysLoginTT.EN
+{
+    internal class Reservas
+    {
+        public int ReservaID { get; set; }
+        public int UserID { get; set; }
+        public int GuiaID { get; set; }
+        public DateTime Fecha { get; set; }
+        public string EstadoReserva  { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
