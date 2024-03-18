788757590971440a142f8e1ca1c232a4f024a94a
katherineodalys24 <katherineodalys24@outlook.es>
2024-03-18 16:52:54 -0600

    comit

---
+++ TOURTRADE.SysLoginTT/TOURTRADE.SysLoginTT.EN/Lugar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOURTRADE.SysLoginTT.EN
+{
+    internal class Lugar
+    {
+        public int LugarID { get; set; }
+        public string NombreLugar { get; set; }
+        public string Descripcion { get; set; }
+        public int CategoriaID { get; set; }
+        public string Ubicacion { get; set; }
+        public List<string> Fotos { get; set; }
+    }
+}
