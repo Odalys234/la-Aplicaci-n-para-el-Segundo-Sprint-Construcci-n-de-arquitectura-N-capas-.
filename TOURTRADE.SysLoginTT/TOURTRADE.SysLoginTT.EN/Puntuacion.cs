5eae33fb657b747114f8a888be11f9a1b25a9f52
katherineodalys24 <katherineodalys24@outlook.es>
2024-03-18 16:53:12 -0600

    comit

---
+++ TOURTRADE.SysLoginTT/TOURTRADE.SysLoginTT.EN/Puntuacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOURTRADE.SysLoginTT.EN
+{
+    internal class Puntuacion
+    {
+        public int ComentarioID { get; set; }
+        public int UserID { get; set; }
+        public int GuiaID { get; set; }
+        public string Comentario { get; set; }
+        public int puntuacion { get; set; }
+    }
+}
